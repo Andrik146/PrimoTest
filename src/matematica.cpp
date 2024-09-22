6781cd5ff454594be648b09a266bbe3ad2103630
Andrea Ansastasi <anastasiandreamail@gmail.com>
2024-09-22 03:26:12 +0200

    Salvataggio nuove librerie

---
+++ src/matematica.cpp
@@ -0,0 +1,10 @@
+//
+// Created by utente on 22/09/2024.
+//
+
+#include <matematica.h>
+
+
+double quadrato (double num) {
+  return num * num;
+}
---
+++ src/matematica.h
@@ -0,0 +1,13 @@
+//
+// Created by utente on 22/09/2024.
+//
+
+#ifndef MATEMATICA_H
+#define MATEMATICA_H
+
+
+double quadrato(double num); //Restituisce il quadrato del numero in input
+
+
+
+#endif //MATEMATICA_H
