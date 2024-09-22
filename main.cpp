7705f066476c3a95461ef1da4319fd5a3625b93b
Andrea Ansastasi <anastasiandreamail@gmail.com>
2024-09-22 03:27:13 +0200

    git dio porco

--- main.cpp
+++ main.cpp
@@ -3,9 +3,19 @@
 //
 
 #include<iostream>
+#include<matematica.h>
+
+using namespace std;
 
 
 int main () {
-    std::cout << "Hello World!\n";
+    double num1;
+
+    cout << "Inserisci il numero di cui desideri calcolare il quadrato:" <<endl;
+    cin >> num1;
+    cout << "Il quadrato vale:" << quadrato(num1);
     return 0;
+
+
+
 }
